428b0251e70cae716b8851c035a5712edec25736
Mohammed Emara <moemara034@gmail.com>
2024-11-10 21:49:05 +0200

    intial commit5

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import list_books , LibraryDetailView
 urlpatterns = [
-    path('',views.list_book, name = "list"),
-    path("library/", views.details_library.as_view(), name = "library")
+    path('',list_book, name = "list"),
+    path("library/", LibraryDetailView.as_view(), name = "library")
 
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,7 +8,7 @@ def list_book(request):
     return render(request,"relationship_app/list_books.html", {"books": books})
 
 
-class details_library(DetailView):
+class LibraryDetailView(DetailView):
 
     model = Library 
     template_name = "relationship_app/library_detail.html"
