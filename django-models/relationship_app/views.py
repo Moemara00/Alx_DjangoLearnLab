e5bcc51ddee61587981903de6e393b2e98c1f291
Mohammed Emara <moemara034@gmail.com>
2024-11-10 21:47:08 +0200

    intial commit4

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Library, Book
-from django.views.generic import ListView , DetailView
+from django.views.generic.detail import DetailView 
 
 def list_book(request):
     books = Book.objects.all()
@@ -8,7 +8,7 @@ def list_book(request):
     return render(request,"relationship_app/list_books.html", {"books": books})
 
 
-class details_library(ListView):
+class details_library(DetailView):
 
     model = Library 
     template_name = "relationship_app/library_detail.html"
