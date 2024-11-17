f578550a4a0b30483b5fc4bbce4811d93c0cdbf5
Mohammed Emara <moemara034@gmail.com>
2024-11-17 16:40:46 +0200

    intial commit4

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .models import CustomUser
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = (None, {'fields': ('email','first_name','last_name')}),
+                ('permissions', {'fields': ('is_staff', 'is_superuser')}),
+                ('Personal', {'fields': ('date_of_birth', 'profile_photo')})
+
+admin.site.register(CustomUser)
