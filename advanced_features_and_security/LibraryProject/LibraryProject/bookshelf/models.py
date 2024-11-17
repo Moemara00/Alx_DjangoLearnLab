a6a97f252ad31b6f287056f30dd29d03b6a59e5f
Mohammed Emara <moemara034@gmail.com>
2024-11-17 15:57:03 +0200

    intial commit3

--- advanced_features_and_security/LibraryProject/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 from django.utils import timezone
 # Create your models here.
@@ -13,22 +13,8 @@ class Book(models.Model):
 
 # bookshelf/models.py
 
-class New_UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
-
-class CustomUser(AbstractUser):
+class New_User(AbstractBaseUser):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -45,3 +31,19 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
 
+
+
+class New_UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
