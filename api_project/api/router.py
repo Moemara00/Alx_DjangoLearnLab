adbb9ce7b2e6ed76253383967a3dbcb083f17d8f
Mohammed Emara <moemara034@gmail.com>
2024-11-30 19:37:38 +0200

    intial commit-

---
+++ api_project/api/router.py
@@ -0,0 +1,5 @@
+from rest_framework.routers import DefaultRouter 
+from .views import BookViewSet
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+urlpatterns = router.urls
--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta: 
-        Model = Book
+        model = Book
         fields = [
+            'pk',
             'title',
             'author',
 
